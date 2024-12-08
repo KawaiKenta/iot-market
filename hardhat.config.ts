a8bde627013e0256dee4b9f6933e99d45c5a90b4
KawaiKenta <kawaken@ertl.jp>
2024-12-08 16:13:29 +0900

    delete: sepholiaの情報を削除

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,6 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
-const EHTERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const SEPOLIA_USER_0 = process.env.SEPOLIA_USER_0 || "";
-const SEPOLIA_USER_1 = process.env.SEPOLIA_USER_1 || "";
-const SEPOLIA_USER_2 = process.env.SEPOLIA_USER_2 || "";
-
 const getCurrentTime = () => {
   const date = new Date();
   return date.toLocaleString("sv-SE");
@@ -22,14 +16,6 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [SEPOLIA_USER_0, SEPOLIA_USER_1, SEPOLIA_USER_2],
-      chainId: 11155111,
-    },
-  },
-  etherscan: {
-    apiKey: EHTERSCAN_API_KEY,
   },
   sourcify: {
     enabled: true,
